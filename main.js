7893db3f734550c64d312e1cb701292ac907b210
Courtney Brothers <cournteywbrothers@gmail.com>
2017-12-01 14:31:57 -0600

    append colored converted temp buttons to the DOM

--- main.js
+++ main.js
@@ -11,6 +11,17 @@ function toCelsius (valueToConvert) {
     console.log('im here now');
     let convertedToCelsius = (inputToConvert - celsiusBase) * (5/9);
     console.log(convertedToCelsius);
+    var celsiusButton = document.createElement("button");
+    celsiusButton.innerHTML = convertedToCelsius;
+    var body = document.getElementsByTagName("body")[0];
+    body.appendChild(celsiusButton);
+    if (convertedToCelsius > 32){
+        celsiusButton.style.background = `#ff0000`;
+    } else if (convertedToCelsius < 0) {
+        celsiusButton.style.background = `#0000ff`;
+    } else {
+        celsiusButton.style.background = `#008000`;
+    }
 
     }
     
@@ -22,6 +33,18 @@ function toFahrenheit (valueToConvert) {
     let convertedToFarenheit = (inputToConvert * (9/5) + (3/2));
     console.log(convertedToFarenheit);
     
+    var fahrenheitButton = document.createElement("button");
+    fahrenheitButton.innerHTML = convertedToFarenheit;
+    var body = document.getElementsByTagName("body")[0];
+    body.appendChild(fahrenheitButton);
+        if (convertedToFarenheit > 90){
+            fahrenheitButton.style.background = `#ff0000`;
+        } else if (convertedToFarenheit < 32) {
+            fahrenheitButton.style.background = `#0000ff`;
+        } else {
+            fahrenheitButton.style.background = `#008000`;
+        }
+        
     }
     
     // Get a reference to the button element in the DOM
